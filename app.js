461d2b3f4bcbedbd89dfe2430910647d1e8a5295
Romain <romainaubry85@gmail.com>
2020-07-01 10:10:46 +0200

    pmremier ajout

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-http://data.fixer.io/api/latest?access_key=c77a840597a7934b06ef954af80a337c&base=EUR&symbols=GBP,JPY,EUR,USD
+var url = "http://data.fixer.io/api/latest?access_key=c77a840597a7934b06ef954af80a337c&base=EUR&symbols=GBP,JPY,EUR,USD"
     
-    
-//console.log(menu.menuUn[0]);
+
+function recupererValeur(){
+    $.ajax({
+        url: url,
+        type : "GET",
+        success: function(res, status, req){
+            console.log("res: ", res.rates.USD);
+            console.log("req: ", req);
+
+        } // Callback
+        
+            })
+}
